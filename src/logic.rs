7f1f5061db1ad17efc142ea04db8efd499a1a846
XPredictMarket <official@x-predict.com>
2021-08-20 18:20:51 +0800

    autonomy

--- src/logic.rs
+++ src/logic.rs
@@ -6,7 +6,7 @@ use std::{
 use subxt::{sp_core::sr25519::Pair as Sr25519Pair, sudo::*, system::*, Client, PairSigner};
 
 use crate::{
-	pallets::{couple::*, proposals::*, tokens::*, *},
+	pallets::{autonomy::*, couple::*, proposals::*, tokens::*, *},
 	runtime::XPredictRuntime,
 };
 
@@ -79,7 +79,7 @@ impl XPredictLogic {
 	) -> Result<ProposalId, Box<dyn std::error::Error>> {
 		let close_time = SystemTime::now()
 			.duration_since(SystemTime::UNIX_EPOCH)?
-			.checked_add(Duration::from_secs(15 * 60)) // 15 min
+			.checked_add(Duration::from_secs(11 * 60)) // 11 min
 			.ok_or("unknown error")?
 			.as_millis() as Moment;
 		let result = client
@@ -91,7 +91,7 @@ impl XPredictLogic {
 				1,
 				currency_id,
 				number,
-				200,
+				2000,
 				"".as_bytes().to_vec(),
 			)
 			.await?;
@@ -116,6 +116,137 @@ impl XPredictLogic {
 			Ok(())
 		} else {
 			Err("unknown error".into())
+		}
+	}
+
+	pub async fn proposal_buy(
+		client: &Client<XPredictRuntime>,
+		signer: &PairSigner<XPredictRuntime, Sr25519Pair>,
+		proposal_id: ProposalId,
+		currency_id: CurrencyId,
+		number: Balance,
+	) -> Result<Balance, Box<dyn std::error::Error>> {
+		let result = client
+			.buy_and_watch(signer, proposal_id, currency_id, number)
+			.await?;
+		if let Some(event) = result.buy()? {
+			Ok(event.number)
+		} else {
+			Err("unknown error".into())
+		}
+	}
+
+	// pub async fn proposal_retrieval(
+	// 	client: &Client<XPredictRuntime>,
+	// 	signer: &PairSigner<XPredictRuntime, Sr25519Pair>,
+	// 	proposal_id: ProposalId,
+	// 	currency_id: CurrencyId,
+	// 	number: Balance,
+	// ) -> Result<Balance, Box<dyn std::error::Error>> {
+	// 	let result = client
+	// 		.retrieval_and_watch(signer, proposal_id, currency_id, number)
+	// 		.await?;
+	// 	if let Some(event) = result.retrieval()? {
+	// 		Ok(event.number)
+	// 	} else {
+	// 		Err("unknown error".into())
+	// 	}
+	// }
+
+	pub async fn autonomy_stake(
+		client: &Client<XPredictRuntime>,
+		signer: &PairSigner<XPredictRuntime, Sr25519Pair>,
+	) -> Result<(), Box<dyn std::error::Error>> {
+		let result = client.stake_and_watch(signer).await?;
+		if result.stake()?.is_some() {
+			Ok(())
+		} else {
+			Err("unknown error".into())
+		}
+	}
+
+	pub async fn autonomy_tag(
+		client: &Client<XPredictRuntime>,
+		signer: &PairSigner<XPredictRuntime, Sr25519Pair>,
+		target: &<XPredictRuntime as System>::AccountId,
+	) -> Result<(), Box<dyn std::error::Error>> {
+		let call = client.encode(TaggingCall { target }).unwrap();
+		let result = client.sudo_and_watch(signer, &call).await?;
+		if result.tagging()?.is_some() {
+			Ok(())
+		} else {
+			Err("unknown error".into())
+		}
+	}
+
+	pub async fn autonomy_minimal_stake_number(
+		client: &Client<XPredictRuntime>,
+	) -> Result<Balance, Box<dyn std::error::Error>> {
+		let result = client.minimal_stake_number(None).await?;
+		if let Some(number) = result {
+			Ok(number)
+		} else {
+			Err("unknown error".into())
+		}
+	}
+
+	pub async fn autonomy_stake_account(
+		client: &Client<XPredictRuntime>,
+		target: &<XPredictRuntime as System>::AccountId,
+	) -> Result<Balance, Box<dyn std::error::Error>> {
+		let result = client.staked_account(target, None).await?;
+		if let Some(number) = result {
+			Ok(number)
+		} else {
+			Ok(0)
+		}
+	}
+
+	pub async fn autonomy_account(
+		client: &Client<XPredictRuntime>,
+		target: &<XPredictRuntime as System>::AccountId,
+	) -> Result<(), Box<dyn std::error::Error>> {
+		client
+			.autonomy_account(target, None)
+			.await?
+			.ok_or_else(|| "unknown error".into())
+	}
+
+	pub async fn proposal_pairs(
+		client: &Client<XPredictRuntime>,
+		proposal_id: ProposalId,
+	) -> Result<(CurrencyId, CurrencyId), Box<dyn std::error::Error>> {
+		let result = client.pool_pairs(proposal_id, None).await?;
+		if let Some(pairs) = result {
+			Ok(pairs)
+		} else {
+			Err("proposal id incorrect".into())
+		}
+	}
+
+	pub async fn proposal_optional(
+		client: &Client<XPredictRuntime>,
+		proposal_id: ProposalId,
+	) -> Result<(Balance, Balance), Box<dyn std::error::Error>> {
+		let result = client
+			.proposal_total_optional_market(proposal_id, None)
+			.await?;
+		if let Some(pairs) = result {
+			Ok(pairs)
+		} else {
+			Err("proposal id incorrect".into())
+		}
+	}
+
+	pub async fn proposal_status(
+		client: &Client<XPredictRuntime>,
+		proposal_id: ProposalId,
+	) -> Result<ProposalStatus, Box<dyn std::error::Error>> {
+		let result = client.proposal_status(proposal_id, None).await?;
+		if let Some(state) = result {
+			Ok(state)
+		} else {
+			Err("proposal id incorrect".into())
 		}
 	}
 
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,9 @@
-use crate::pairs::{PairAuthority, XPredictKeystore, XPredictPairs};
+use std::{thread, time};
+
+use crate::{
+    pairs::{PairAuthority, XPredictKeystore, XPredictPairs},
+    pallets::ProposalStatus,
+};
 use logic::XPredictLogic;
 use runtime::XPredictRuntime;
 use sp_core::Pair;
@@ -26,16 +31,25 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     let max_currency_id = XPredictLogic::number_of_currency(&client).await?;
 
-    let (currency_id, decimals) = if max_currency_id == 1 {
-        let currency_id = XPredictLogic::new_asset(&client, &admin_signer, 8).await?;
-        (currency_id, 8)
+    let (currency_id, decimals) = if max_currency_id <= 2 {
+        let mut currency_id = max_currency_id;
+        let decimals: u8 = 8;
+        while currency_id <= 2 {
+            currency_id = XPredictLogic::new_asset(&client, &admin_signer, decimals).await?;
+        }
+        (currency_id, decimals)
     } else {
-        let token_info = XPredictLogic::currencies(&client, 1).await?;
-        (1, token_info.decimals)
+        let currency_id = 2;
+        let token_info = XPredictLogic::currencies(&client, currency_id).await?;
+        (currency_id, token_info.decimals)
     };
 
+    println!(
+        "use currency: {:?}, decimals: {:?} to make proposal",
+        currency_id, decimals
+    );
     let bob_number = 100 * 10u128.pow(decimals as u32);
-    let charlie_number = 255 * 10u128.pow((decimals - 1) as u32);
+    let charlie_number = 3125 * 10u128.pow((decimals - 2) as u32);
     let number = bob_number + charlie_number;
 
     let balance = XPredictLogic::balance_of(
@@ -45,6 +59,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     )
     .await?;
 
+    println!("waiting for trasfer tokens...");
     if balance < number {
         XPredictLogic::mint_token(
             &client,
@@ -63,9 +78,11 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     .unwrap();
     let bob = pairs_iter.next().unwrap();
     let charlie = pairs_iter.next().unwrap();
+    let dave = pairs_iter.next().unwrap();
+    let eve = pairs_iter.next().unwrap();
 
     for id in [0, currency_id] {
-        for public in [bob.public(), charlie.public()] {
+        for public in [bob.public(), charlie.public(), dave.public(), eve.public()] {
             let balance = XPredictLogic::balance_of(&client, id, &public.into()).await?;
             let num = if id == 0 {
                 if balance > 10u128.pow(12) {
@@ -89,11 +106,71 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         }
     }
 
+    let autonomy_minimal_stake_number =
+        XPredictLogic::autonomy_minimal_stake_number(&client).await?;
+    for account in [dave, eve] {
+        let balance = XPredictLogic::balance_of(&client, 1, &account.public().into()).await?;
+        if balance < autonomy_minimal_stake_number {
+            XPredictLogic::transfer_token(
+                &client,
+                &admin_signer,
+                1,
+                &account.public().into(),
+                autonomy_minimal_stake_number - balance,
+            )
+            .await?;
+        }
+        let _signer = <XPredictKeystore as XPredictPairs<XPredictRuntime>>::get_signer(account);
+        let staked_number =
+            XPredictLogic::autonomy_stake_account(&client, &account.public().into()).await?;
+        if staked_number != autonomy_minimal_stake_number {
+            XPredictLogic::autonomy_stake(&client, &_signer).await?;
+        }
+
+        if XPredictLogic::autonomy_account(&client, &account.public().into())
+            .await
+            .is_err()
+        {
+            XPredictLogic::autonomy_tag(&client, &admin_signer, &dave.public().into()).await?;
+        }
+    }
+
     let bob_signer = <XPredictKeystore as XPredictPairs<XPredictRuntime>>::get_signer(bob);
     let proposal_id =
         XPredictLogic::make_proposal(&client, &bob_signer, currency_id, bob_number).await?;
     println!("make proposal with id : {:?}", proposal_id);
 
     XPredictLogic::quick_to_formal(&client, &admin_signer, proposal_id).await?;
+
+    let charlie_signer = <XPredictKeystore as XPredictPairs<XPredictRuntime>>::get_signer(bob);
+    let (yes, _) = XPredictLogic::proposal_pairs(&client, proposal_id).await?;
+    let actual_number =
+        XPredictLogic::proposal_buy(&client, &charlie_signer, proposal_id, yes, charlie_number)
+            .await?;
+    println!(
+        "actual: {:?}, fee: {:?}",
+        actual_number,
+        charlie_number - actual_number
+    );
+
+    let balance = XPredictLogic::balance_of(&client, yes, &bob.public().into()).await?;
+
+    println!("bob yes balance: {:?}", balance);
+
+    let (yes, no) = XPredictLogic::proposal_optional(&client, proposal_id).await?;
+    println!("yes: {:?}, no: {:?}", yes, no);
+
+    // stake to node
+
+    println!("waiting for proposal status...");
+    loop {
+        let state = XPredictLogic::proposal_status(&client, proposal_id).await?;
+        println!("current proposal status: {:?}", state);
+        if state == ProposalStatus::WaitingForResults {
+            break;
+        }
+        thread::sleep(time::Duration::from_secs(10));
+    }
+
     Ok(())
 }
